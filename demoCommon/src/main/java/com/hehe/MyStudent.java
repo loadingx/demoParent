6c76c3167b018a04a0b8c3c796c97fbc2e2e5d1c
cuijian <15652359335@163.com>
2020-07-10 13:36:06 +0800

    eeee

--- demoCommon/src/main/java/com/hehe/MyStudent.java
+++ demoCommon/src/main/java/com/hehe/MyStudent.java
@@ -1,4 +1,15 @@
 package com.hehe;
 
-public class MyStudent {
+import java.io.Serializable;
+
+public class MyStudent implements Serializable {
+
+
+    private static final long serialVersionUID = -1986807309366866757L;
+
+    private String name;
+    private String password;
+
+
+
 }
