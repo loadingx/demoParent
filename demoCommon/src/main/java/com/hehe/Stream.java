e1f78c369817159620f0af1232bcb274dfefb54c
cuijian <15652359335@163.com>
2020-07-13 13:59:20 +0800

    Stream

---
+++ demoCommon/src/main/java/com/hehe/Stream.java
@@ -0,0 +1,19 @@
+package com.hehe;
+
+import java.util.ArrayList;
+
+public class Stream {
+
+    public static void main(String[] args) {
+
+
+        ArrayList<Object> list = new ArrayList();
+
+        list.add("hehe");
+        list.add("xxxx");
+        list.add("222");
+
+    }
+
+
+}
